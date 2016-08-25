094871967a7534d7888d416346da2f8da96362a1
elzup <guild0105@gmail.com>
2016-08-25 17:57:04 +0900

    Update logs structure

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const test = speedTest({maxTime: 5000})
 test.on('data', data => {
     const up = data.speeds.originalUpload
     const down = data.speeds.originalDownload
-    db.run('INSERT INTO log (up, down) VALUES(' + up + ',  ' + down + ')');
+    db.run('INSERT INTO logs (up, down) VALUES(' + up + ',  ' + down + ')');
 })
--- migrate.js
+++ migrate.js
@@ -1,5 +1,12 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('db/database.db');
 
-db.run('CREATE TABLE log (id, up, down, created_at)');
+const sql = '' +
+    'CREATE TABLE logs ('
+    + 'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+    + 'up INTEGER,'
+    + 'down INTEGER,'
+    + 'created_at DEFAULT CURRENT_TIMESTAMP)'
+
+db.run(sql);
 db.close();
