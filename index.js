0fb223133e0d86fb5f4de8ed2da9e9e83e513100
elzup <guild0105@gmail.com>
2016-08-25 18:36:00 +0900

    Fix DB connection close

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const speedTest = require('speedtest-net')
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('db/database.db');
 
 const test = speedTest({maxTime: 5000})
 
 test.on('data', data => {
+    const db = new sqlite3.Database('db/database.db');
     const up = data.speeds.originalUpload
     const down = data.speeds.originalDownload
     db.run('INSERT INTO logs (up, down) VALUES(' + up + ',  ' + down + ')');
+    db.close();
 })
