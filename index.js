67c38be6f16c550a4074be377228f218846c5b0f
elzup <guild0105@gmail.com>
2016-08-26 13:26:26 +0900

    Add editorconfig Format!

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const path = require('path')
 const dbPath = path.resolve(__dirname, 'db', 'database.db')
 
 const test = speedTest({
-    maxTime: 100,
-    serversUrl: 'http://www.speedtest.net/speedtest-servers-static.php?r=' + Math.random()
+	maxTime: 100,
+	serversUrl: 'http://www.speedtest.net/speedtest-servers-static.php?r=' + Math.random()
 })
 
 test.on('data', data => {
-    const db = new sqlite3.Database(dbPath);
-    const up = data.speeds.originalUpload
-    const down = data.speeds.originalDownload
-    db.run('INSERT INTO logs (up, down) VALUES(' + up + ',  ' + down + ')');
-    db.close();
+	const db = new sqlite3.Database(dbPath);
+	const up = data.speeds.originalUpload
+	const down = data.speeds.originalDownload
+	db.run('INSERT INTO logs (up, down) VALUES(' + up + ',  ' + down + ')');
+	db.close();
 })
--- migrate.js
+++ migrate.js
@@ -4,11 +4,11 @@ const dbPath = path.resolve(__dirname, 'db', 'database.db')
 const db = new sqlite3.Database(dbPath);
 
 const sql = '' +
-    'CREATE TABLE logs ('
-    + 'id INTEGER PRIMARY KEY AUTOINCREMENT,'
-    + 'up INTEGER,'
-    + 'down INTEGER,'
-    + 'created_at DEFAULT CURRENT_TIMESTAMP)'
+	'CREATE TABLE logs ('
+	+ 'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+	+ 'up INTEGER,'
+	+ 'down INTEGER,'
+	+ 'created_at DEFAULT CURRENT_TIMESTAMP)'
 
 db.run(sql);
 db.close();
